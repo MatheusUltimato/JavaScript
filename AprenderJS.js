4869577e29f893a6e075b1685f5176863b14152c
MatheusUltimato <mleao782@gmail.com>
2026-03-03 09:41:37 -0300

    Add files via upload

---
+++ AprenderJS.js
@@ -0,0 +1,30 @@
+// Aqui você vai aprender JavaScript
+
+/*Primeiros comandos */
+console.log() /*Comando basico para gerar algo no código[texto/número]*/
+
+// = Serve para fazer um Título principal para um tema específico de relevância
+// /* */ =Serve para adicionar comentários no código[anotações]
+
+//Dados fundamentais:
+/*
+Number = Números
+Bigint = Inteiro
+String = Texto
+Boolean = Verdadeiro/Falso [True/False]
+Null = Nulo
+Undefined = Indefinido
+Symbol = Símbolo
+Object = Objeto
+*/
+// Operadores Matemáticos
+/* 
+1.Adição +
+2.Subtração -
+3.Multiplicação *
+4.Divisão /
+*/
+
+// Comandos
+
+
